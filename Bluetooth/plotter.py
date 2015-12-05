1023bffbf782138e9aa2e58f6a1996d4b8fca3da
Alexander DeRieux <zanderieux@DeRieux-MacBook-Pro.local>
2015-12-05 18:39:06 -0500

    Added script for generating XY plots.

---
+++ Bluetooth/plotter.py
@@ -0,0 +1,33 @@
+
+###
+# Imported Libraries.
+###
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+def generatePlot( x, y, title, xlabel, ylabel, name, ext ):
+	"""Create and save an XY plot.
+
+	Generates an connected XY plot using the given data.
+	"""
+
+	try:
+		# Set plot labels
+		plt.figure( name )
+		plt.ylabel( ylabel )
+		plt.xlabel( xlabel )
+
+		# Plot the data.
+		plt.plot( x, y )
+
+		# Save the figure.
+		plt.savefig( name + "." + ext )
+
+		# Return the name of the plot.
+		return name + "." + ext
+
+	except:
+		# Plot failure.
+		return None
