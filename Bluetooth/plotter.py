4d9113c53b17264c4fba0c96b7e46f2b6f0c3617
Alexander DeRieux <zanderieux@DeRieux-MacBook-Pro.local>
2015-12-07 16:58:02 -0500

    Updated plotting method names and content.

--- Bluetooth/plotter.py
+++ Bluetooth/plotter.py
@@ -7,8 +7,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def generate2DLinePlot( x, y, title, xlabel, ylabel, name, ext ):
-	"""Create and save an XY plot.
+def generateLinePlot( x, y, title, xlabel, ylabel, name, ext ):
+	"""Create and save an XY Line plot.
 
 	Generates an connected XY plot using the given data.
 	"""
@@ -27,6 +27,20 @@ def generate2DLinePlot( x, y, title, xlabel, ylabel, name, ext ):
 
 		# Return the name of the plot.
 		return name + "." + ext
+
+	except:
+		# Plot failure.
+		return None
+
+def generateBarPlot( title, xlabel, ylabel, name, ext ):
+
+	"""Create and save an XY Bar plot.
+
+	Generates an XY Bar plot using the given data.
+	"""
+
+	try:
+		return "blank" 
 
 	except:
 		# Plot failure.
